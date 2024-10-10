c08fe5ecf0b4681f0987f76a7d7f0d6e34ebd8a3
Aryan <aryanesmailiea@gmail.com>
2024-10-11 00:57:41 +0330

    base project has been setup main layout written. Home page written.

--- URLShortenerBlazor/Program.cs
+++ URLShortenerBlazor/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using URLShortenerBlazor;
+using Blazored.LocalStorage; 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddBlazoredLocalStorage(); 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
