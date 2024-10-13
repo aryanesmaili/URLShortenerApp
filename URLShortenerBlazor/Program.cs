7cdd98b9917a1911d25696d42ee804d9272681d8
Aryan <aryanesmailiea@gmail.com>
2024-10-13 18:48:13 +0330

    new Home page written

--- URLShortenerBlazor/Program.cs
+++ URLShortenerBlazor/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using URLShortenerBlazor;
-using Blazored.LocalStorage; 
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddBlazoredLocalStorage(); 
