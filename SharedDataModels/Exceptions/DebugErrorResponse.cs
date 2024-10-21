17ada2c48a6a8d8f2c383bf8ae3fe3475b20e516
Aryan <aryanesmailiea@gmail.com>
2024-10-21 21:59:10 +0330

    Exceptions added to sharedDataModel in case they're needed in front-end later

---
+++ SharedDataModels/Exceptions/DebugErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedDataModels.Utility.Exceptions
+{
+    internal class DebugErrorResponse
+    {
+        public string Message { get; set; }
+        public string InnerException { get; set; }
+        public string StackTrace { get; set; }
+    }
+}
---
+++ SharedDataModels/Exceptions/NotAuthorizedException.cs
@@ -0,0 +1,18 @@
+﻿namespace SharedDataModels.Utility.Exceptions
+{
+    public class NotAuthorizedException : Exception
+    {
+        public NotAuthorizedException()
+        {
+
+        }
+        public NotAuthorizedException(string Message) : base(Message)
+        {
+
+        }
+        public NotAuthorizedException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
