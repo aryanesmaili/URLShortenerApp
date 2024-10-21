78f1a5cb166899da0910f3f3bf735016b3992f8e
Aryan <aryanesmailiea@gmail.com>
2024-10-21 22:01:43 +0330

    added validation for list of url create DTOs

--- URLShortenerAPI/Utility/ValidatorConfigs/URLCreateValidation.cs
+++ URLShortenerAPI/Utility/ValidatorConfigs/URLCreateValidation.cs
@@ -10,8 +10,15 @@ namespace URLShortenerAPI.Utility.ValidatorConfigs
             RuleFor(x => x.LongURL)
                 .NotEmpty().WithMessage("URL field cannot be empty.")
                 .Matches(@"^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+([\/\w-]*)*\/?$").WithMessage("Given string is not a URL"); // Regex to determine URLs.
-            RuleFor(x => x.IsActive).NotEmpty().WithMessage("IsActive field cannot be empty.");
+            RuleFor(x => x.IsActive).NotNull().WithMessage("IsActive field cannot be empty.");
             RuleFor(x => x.UserID).NotEmpty().WithMessage("The URL must belong to a user.");
+        }
+    }
+    public class ListURLCreateValidation : AbstractValidator<List<URLCreateDTO>>
+    {
+        public ListURLCreateValidation()
+        {
+            RuleForEach(x => x).SetValidator(new URLCreateValidation());
         }
     }
 }
