e8ba10158e21791819b7fb0d0103ef64af72a58d
Aryan <aryanesmailiea@gmail.com>
2024-10-30 22:48:46 +0330

    new db scheme

--- URLShortenerAPI/Migrations/20241030130535_fixdeviceinfo.Designer.cs
+++ URLShortenerAPI/Migrations/20241030130535_fixdeviceinfo.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using URLShortenerAPI.Data;
@@ -11,9 +12,11 @@ using URLShortenerAPI.Data;
 namespace URLShortenerAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241030130535_fixdeviceinfo")]
+    partial class fixdeviceinfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ URLShortenerAPI/Migrations/20241030130535_fixdeviceinfo.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URLShortenerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixdeviceinfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserAgent",
+                table: "DeviceInfos",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OSFamily",
+                table: "DeviceInfos",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BrowserFamily",
+                table: "DeviceInfos",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Client_PreventNull",
+                table: "DeviceInfos",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Device_PreventNull",
+                table: "DeviceInfos",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "OS_PreventNull",
+                table: "DeviceInfos",
+                type: "boolean",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Client_PreventNull",
+                table: "DeviceInfos");
+
+            migrationBuilder.DropColumn(
+                name: "Device_PreventNull",
+                table: "DeviceInfos");
+
+            migrationBuilder.DropColumn(
+                name: "OS_PreventNull",
+                table: "DeviceInfos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserAgent",
+                table: "DeviceInfos",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OSFamily",
+                table: "DeviceInfos",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BrowserFamily",
+                table: "DeviceInfos",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
