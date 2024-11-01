5b9ffabdeb78d137139b18c5d4365a5b6e70024f
Aryan <aryanesmailiea@gmail.com>
2024-11-01 05:10:09 +0330

    validation for user email change request and password change request is added

--- URLShortenerAPI/Utility/ValidatorConfigs/UserCreateValidator.cs
+++ URLShortenerAPI/Utility/ValidatorConfigs/UserCreateValidator.cs
@@ -44,15 +44,23 @@ namespace URLShortenerAPI.Responses.ValidatorConfigs
         public UserUpdateValidator(IUserService userService)
         {
             _userService = userService;
-            RuleFor(x => x.Name).MinimumLength(4).When(x => x.Name.Length > 0);
+            RuleFor(x => x.Name).MinimumLength(4).When(x => x.Name?.Length > 0);
 
-            RuleFor(x => x.Email)
-                .EmailAddress().When(x => !string.IsNullOrEmpty(x.Email))
+            RuleFor(x => x.Username).Must(x => !_userService.IsUser(x)).When(x => x.Username?.Length > 0);
+        }
+    }
+    public class UserChangeEmailValidator : AbstractValidator<ChangeEmailRequest>
+    {
+        private readonly IUserService _userService;
+        public UserChangeEmailValidator(IUserService userService)
+        {
+            _userService = userService;
+
+            RuleFor(x => x.NewEmail)
+                .EmailAddress().When(x => !string.IsNullOrEmpty(x.NewEmail))
                 .WithMessage("Invalid email format.")
-                .Must(email => !_userService.IsEmailTaken(email)).When(x => !string.IsNullOrEmpty(x.Email))
+                .Must(email => !_userService.IsEmailTaken(email)).When(x => !string.IsNullOrEmpty(x.NewEmail))
                 .WithMessage("Email is already taken.");
-
-            RuleFor(x => x.Username).Must(x => !_userService.IsUser(x)).When(x => x.Username.Length > 0);
         }
     }
     public class UserLoginDTOValidator : AbstractValidator<UserLoginDTO>
@@ -61,6 +69,24 @@ namespace URLShortenerAPI.Responses.ValidatorConfigs
         {
             RuleFor(x => x.Identifier).NotEmpty();
             RuleFor(x => x.Password).NotEmpty().MinimumLength(5);
+        }
+    }
+    public class ChangePasswordValidator : AbstractValidator<ChangePasswordRequest>
+    {
+        private readonly IUserService _userService;
+        public ChangePasswordValidator(IUserService userService)
+        {
+            _userService = userService;
+            RuleFor(x => x.UserInfo).NotEmpty().Must(x => _userService.IsUser(x.ID)).WithMessage($"Invalid User");
+
+            RuleFor(x => x.NewPassword).NotEmpty()
+                .MinimumLength(5).WithMessage("Your Password should have at least 5 Characters")
+                .MaximumLength(64).WithMessage("Your Password should have at most 64 Characters");
+
+            RuleFor(x => x.ConfirmPassword).NotEmpty()
+                .MinimumLength(5).WithMessage("Your Password should have at least 5 Characters")
+                .MaximumLength(64).WithMessage("Your Password should have at most 64 Characters")
+                .Equal(x => x.NewPassword).WithMessage("entered Passowrd fields are not the same.");
         }
     }
 }
