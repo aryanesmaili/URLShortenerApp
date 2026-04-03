0c0d97dad96b3a9c4e604aaac0c16a0702f59cfb
Aryan <aryanesmailiea@gmail.com>
2026-04-03 09:51:47 +0330

    cleaned up email service by moving reusable objects to class-level fields to reduce the amount of operations on each send.

--- URLShortener.Infrastructure/Services/User/EmailService.cs
+++ URLShortener.Infrastructure/Services/User/EmailService.cs
@@ -1,39 +1,44 @@
-﻿using Microsoft.Extensions.Options;
-using System.Net;
+﻿using System.Net;
 using System.Net.Mail;
 using URLShortener.Application.DTOs.Settings;
 using URLShortener.Application.Interfaces.Infrastructure.External;
 
 namespace URLShortener.Infrastructure.Services.User
 {
-    public class EmailService(IOptions<SMTPSettings> smtpSettings) : IEmailService
+    public class EmailService : IEmailService
     {
-        private readonly SMTPSettings _smtpSettings = smtpSettings.Value;
+        private readonly SMTPSettings _smtpSettings;
+        private readonly SmtpClient _smtpClient;
+        private readonly MailAddress _mailAddress;
+        public EmailService(SMTPSettings smtpSettings)
+        {
+            _smtpSettings = smtpSettings;
+            _smtpClient = new SmtpClient(_smtpSettings.Server, _smtpSettings.Port)
+            {
+                Credentials = new NetworkCredential(_smtpSettings.Username, _smtpSettings.Password),
+                EnableSsl = true
+            };
+            _mailAddress = new MailAddress(_smtpSettings.SenderEmail, _smtpSettings.SenderName);
+        }
 
         /// <summary>
-        /// Sends an Email using the SMTP settings provided in AppSettings.json.
+        /// Sends an Email using the SMTP settings provided in Config file.
         /// </summary>
         /// <param name="to">receiver of the email.</param>
         /// <param name="subject">Subject of the email.</param>
         /// <param name="body">Body of the email.</param>
         public async Task SendEmail(string to, string subject, string body)
         {
-            SmtpClient smtpClient = new SmtpClient(_smtpSettings.Server, _smtpSettings.Port)
+            MailMessage mailMessage = new()
             {
-                Credentials = new NetworkCredential(_smtpSettings.Username, _smtpSettings.Password),
-                EnableSsl = true
-            };
-
-            MailMessage mailMessage = new MailMessage
-            {
-                From = new MailAddress(_smtpSettings.SenderEmail, _smtpSettings.SenderName),
+                From = _mailAddress,
                 Subject = subject,
                 Body = body,
                 IsBodyHtml = false,
             };
-
             mailMessage.To.Add(to);
-            await smtpClient.SendMailAsync(mailMessage);
+
+            await _smtpClient.SendMailAsync(mailMessage);
         }
     }
 }
