d99df66849bc7eba631a51d7a62d7ae125f93575
Aryan <aryanesmailiea@gmail.com>
2026-04-03 14:12:01 +0330

    added support for paginated loading.

---
+++ URLShortener.Persistence/Repositories/GetPaged.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using URLShortener.Application.Repositories;
+using URLShortener.Application.Utility;
+using URLShortener.Common.Responses;
+using URLShortenerAPI.Data;
+namespace URLShortener.Persistence.Repositories;
+
+public sealed class GetPaged<T> : IGetPaged<T> where T : class
+{
+    private readonly AppDbContext _dbContext;
+
+    public GetPaged(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<PagedResult<T>> GetPagedAsync<TKey>(
+        int pageNumber,
+        int pageSize,
+        Expression<Func<T, bool>>? ownershipFilter = null,
+        Expression<Func<T, TKey>>? orderBy = null,
+        bool descending = false)
+    {
+        var set = _dbContext.Set<T>().AsQueryable();
+
+        if (ownershipFilter != null)
+            set = set.Where(ownershipFilter);
+
+        if (orderBy != null)
+        {
+            set = descending
+                ? set.OrderByDescending(orderBy)
+                : set.OrderBy(orderBy);
+        }
+
+        var totalCount = await set.CountAsync();
+
+        var items = await set
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+
+        return CreateResult.CreatePagedSuccess(
+            items,
+            pageNumber,
+            pageSize,
+            totalCount,
+            totalPages
+        );
+    }
+
+}
+
